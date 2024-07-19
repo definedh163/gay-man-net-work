72d5fff608a7e66386d158598ad6e1b2a4a1fcc2
黄潮鑫 <definedh@163.com>
2024-07-19 14:29:13 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <TableView />
+</template>
+
+<script>
+import TableView from "./components/table-view.vue";
+
+export default {
+  name: "App",
+  components: {
+    TableView,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  padding: 0 10%;
+}
+</style>
